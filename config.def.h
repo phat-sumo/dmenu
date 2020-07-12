947ff5a79ea939e7f48f93dcd5490687b648c7ee
phat_sumo <ethanjpayne1@gmail.com>
2020-07-12 13:52:21 -0600

    add centered grid layout

--- config.def.h
+++ config.def.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -14,9 +16,11 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#262626", "#ff87d7" },
   [SchemeHp] = { "#262626", "#87ffff" }
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 38;         /* -h option; minimum height of a menu line     */
+static unsigned int columns    = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 38;         
 
 /*
  * Characters not considered part of a word while deleting words
