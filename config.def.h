370842a78cc687292312557610e5341bcb02de45
phat_sumo <ethanjpayne1@gmail.com>
2020-07-12 16:18:09 -0600

    add border

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -17,8 +17,8 @@ static const char *colors[SchemeLast][2] = {
   [SchemeHp] = { "#262626", "#87ffff" }
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 3;
+static unsigned int columns    = 3;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 38;         
 
@@ -27,3 +27,6 @@ static unsigned int lineheight = 38;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 1;
