c390271af699374cddf7c8cedee481a52c32e9ad
phat_sumo <ethanjpayne1@gmail.com>
2023-11-02 00:48:21 -0600

    update font specification to use pixelsize

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=8"
+ "monospace:style=Retina:pixelsize=10:antialias=true:autohint=false"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -20,7 +20,7 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 3;
 static unsigned int columns    = 3;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 38;         
+static unsigned int lineheight = 38;
 
 /*
  * Characters not considered part of a word while deleting words
